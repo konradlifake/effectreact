bc5f2e5fc4195078e645235134c15d86ff16c565
konradlifake <konradpatryk2009@gmail.com>
2026-07-17 12:31:33 +0100

    zegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  useEffect
+  const [time, setTime] = useState(new Date());
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerRef.current);
+  }, []);
+
   return (
-    <h1></h1>
+    <h1>{time.toLocaleTimeString()}</h1>
   );
 }
 
