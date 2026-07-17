e86abda0cabda674b2d854318386b16b2c80f584
konradlifake <konradpatryk2009@gmail.com>
2026-07-17 13:19:52 +0100

    apiz ludzmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [time, setTime] = useState(new Date());
-  const timerRef = useRef(null);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    timerRef.current = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timerRef.current);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
   }, []);
 
   return (
-    <h1>{time.toLocaleTimeString()}</h1>
+    <div>
+      <h1>Users</h1>
+      {users.map((user) => (
+        <>
+        <h2 key={user.id}>{user.name}</h2>
+        <p>Email : {user.email}</p>
+        <p>username : {user.username}</p>
+        <p>address : {user.address.street}, {user.address.suite} , {user.address.city}, {user.address.zipcode}</p>
+        <p>Geo location :  {user.address.geo.lat}, {user.address.geo.lng}</p>
+        </>
+      ))}
+
+    </div>
   );
 }
 
